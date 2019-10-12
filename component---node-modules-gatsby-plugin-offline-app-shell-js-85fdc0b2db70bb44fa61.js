8060aea8b65fb56421adf9e43f284cc99b0d96cd
Juan Carlos Farah <farah.juancarlos@gmail.com>
2019-10-12 13:12:38 +0200

    Updates

---
+++ component---node-modules-gatsby-plugin-offline-app-shell-js-85fdc0b2db70bb44fa61.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{499:function(e,t,n){"use strict";var u=n(46);t.__esModule=!0,t.default=void 0;var o=u(n(144)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-85fdc0b2db70bb44fa61.js.map
